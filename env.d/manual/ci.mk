113f7445797321ed811578364edbc6decd32c22b
hashiudo <79960713+lbv180620@users.noreply.github.com>
2022-08-21 09:36:58 +0900

    Initial commit

---
+++ env.d/manual/ci.mk
@@ -0,0 +1,83 @@
+# ==== CI/CDパイプライン関連 ====
+
+# **** CircleCI ****
+
+# https://circleci.com/docs/ja
+
+# Orbs
+# https://circleci.com/developer/ja/orbs
+
+# 認証画面
+# https://circleci.com/ja/vcs-authorize/
+
+# 記事
+# https://qiita.com/kodokunadancer/items/cda4747a10924af476a2
+# https://qiita.com/gold-kou/items/4c7e62434af455e977c2
+
+
+# **** Github Actions ****
+
+# https://docs.github.com/ja/actions
+
+# 記事
+# https://zenn.dev/hashito/articles/7c292f966c0b59
+
+# ? CircleCIとGithub Actionsの比較
+# https://zenn.dev/nus3/articles/44977a5ea4d6cd
+
+
+# ==== CircleCI + Heroku ====
+
+# 記事
+# https://qiita.com/kazumakishimoto/items/6aac32725ebea25acf35
+# https://zenn.dev/ttskch/articles/2598d4acbf342c
+# https://qiita.com/seiya0429/items/83e3f5d53a01d6fbd8d0
+# https://circleci.com/blog/deploy-laravel-heroku/
+# https://blog.nakamu.me/posts/laravel-circleci-github-heroku
+# https://zenn.dev/nagi125/articles/ea1d314c94409341a3b0
+# https://qiita.com/maru401/items/d3f1c0ce3da9cd32770c
+
+
+# ==== Github Actions + Heroku ====
+
+# 記事
+# https://qiita.com/ProjectEuropa/items/33b1054674f117f27bdf
+
+
+# ==== CircleCI + AWS① (CircleCIからAWSのEC2にSSHログインしてデプロイ) ====
+
+# https://www.techpit.jp/courses/78/curriculums/81/sections/619/parts/2119
+# https://github.com/shonansurvivors/laravel-sns/tree/master
+# https://github.com/shonansurvivors/laravel-ci
+
+
+# ==== CircleCI + AWS② (CircleCI + CodeDeployを使ってEC2にデプロイ) ====
+
+# https://www.techpit.jp/courses/78/curriculums/81/sections/620/parts/2127
+# https://github.com/shonansurvivors/laravel-sns/tree/master
+# https://github.com/shonansurvivors/laravel-ci
+
+# ==== CircleCI + AWS③ (CircleCI + CodeCommit + CodeBuild + CodeDeploy + CodePipelineを使ってEC2にデプロイ) ====
+
+# https://www.techpit.jp/courses/78/curriculums/81/sections/621/parts/2140
+# https://github.com/shonansurvivors/laravel-sns/tree/master
+# https://github.com/shonansurvivors/laravel-ci
+
+
+# ==== Github Actions + AWS ====
+
+# 記事
+# https://zenn.dev/tokku5552/articles/laravel-github-actions
+
+# ==== CircleCI + AWS Fargate + Terraform ====
+
+# 記事
+# https://zenn.dev/hibriiiiidge/books/49ee4063b10cec1df1a2
+# https://zenn.dev/endo/books/6c9c8e9e74a3d30bcf08
+# https://www.akashixi-tech.com/laravel/ci-cd
+
+
+# ==== Github Actions + AWS Fargate + Terraform ====
+
+# https://github.com/shonansurvivors/laravel-fargate-app
+# https://github.com/shonansurvivors/laravel-fargate-infra
